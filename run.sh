9114cf75bc51a027e5628147f025c0a3d761b213
Emil VATAI <emil.vatai@gmail.com>
2021-01-07 13:26:14 +0900

    Update `run.sh`

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 SRCDIR=${HOME}/code
+CONTAINER=simulator
 
-docker run --rm -v ${HOME}/code/NEDO/util/polybench-c-3.2:${SRCDIR} riken/simulator \
+docker run --name=${CONTAINER} -v ${HOME}/code/NEDO/util/polybench-c-3.2:${SRCDIR} \
+       riken/simulator \
        aarch64-linux-gnu-gcc-8 -static -O3 \
        -I${SRCDIR}/utilities/ -I${SRCDIR}/stencils/jacobi-1d-imper/ \
        ${SRCDIR}/utilities/polybench.c ${SRCDIR}/stencils/jacobi-1d-imper/jacobi-1d-imper.c \

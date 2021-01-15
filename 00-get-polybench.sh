c0656a7ede961fab04ffd08cfe0972b03c09ea67
Emil VATAI <emil.vatai@gmail.com>
2021-01-15 12:31:45 +0900

    Use polybench v4.2.1-beta

--- 00-get-polybench.sh
+++ 00-get-polybench.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ ! -f ./polybench-c-3.2.tar.gz ]; then
-	wget http://web.cse.ohio-state.edu/\~pouchet.2/software/polybench/download/polybench-c-3.2.tar.gz
+    # wget http://web.cse.ohio-state.edu/\~pouchet.2/software/polybench/download/polybench-c-3.2.tar.gz
+    wget https://downloads.sourceforge.net/project/polybench/polybench-c-4.2.1-beta.tar.gz
 fi
 if [ ! -d ./polybench-c-3.2 ]; then
-	tar xzf polybench-c-3.2.tar.gz
+	# tar xzf polybench-c-3.2.tar.gz
+	tar xzf polybench-c-4.2.1-beta.tar.gz
 fi

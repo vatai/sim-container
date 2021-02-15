b75908387b76a5b1ff905c6f68b2469aaa17de77
Emil VATAI <emil.vatai@gmail.com>
2021-02-15 17:36:33 +0900

    Move polybench inside benchmarks

--- 00-get-polybench.sh
+++ 00-get-polybench.sh
@@ -4,6 +4,8 @@ POLYBENCH_DIR=polybench-c-4.2.1-beta
 POLYBENCH_ARCHIVE=${POLYBENCH_DIR}.tar.gz
 URL_PREFIX=https://downloads.sourceforge.net/project/polybench/
 
+cd benchmarks
+
 if [ ! -f ${POLYBENCH_ARCHIVE} ]; then
     # wget http://web.cse.ohio-state.edu/\~pouchet.2/software/polybench/download/polybench-c-3.2.tar.gz
     wget ${URL_PREFIX}${POLYBENCH_ARCHIVE}

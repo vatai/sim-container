8859ba5d3171cabcc75124bea4f50c7d88f3ac2b
Emil VATAI <emil.vatai@gmail.com>
2021-02-16 22:39:24 +0900

    Doing it better

--- runner.py
+++ runner.py
@@ -13,11 +13,6 @@ COMPILE_CMD = "./02-compile-benchmark.sh"
 SIM_CMD = "./03-run-benchmark.sh"
 
 
-def make_pair(a, b):
-    # return f'{a.upper()}="{b}"'
-    return a.upper(), b
-
-
 def make_list(k, v):
     if isinstance(v, str):
         if Path(v).exists():
@@ -33,47 +28,24 @@ def make_list(k, v):
         raise ValueError
 
 
-def fun2(t):
-    return [make_list(k, v) for k, v in t]
+def to_keyvalue_pairs(dict_with_list_values, key, benchmark):
+    list_of_lists = [make_list(k, v) for k, v in dict_with_list_values.items()]
+    if key:
+        list_of_lists.append([(key, benchmark)])
+    return list(itertools.product(*list_of_lists))
 
 
-def fun1(t):
-    return list(itertools.product(*fun2(t)))
-
-
-def get_sim_params(d):
-    tmp = sum([fun1(t.items()) for t in d], [])
-    for t in tmp:
-        # print("-", list(map(lambda t: make_pair(*t), t)))
-        print("-", t)
-    return tmp
-
-
-def fun4(k, v):
-    tmp = get_sim_params(v)
-    return list(map(lambda t: (("BENCHMARK", k), *t), tmp))
-
-
-def fun3(t):
-    return sum([fun4(k, v) for k, v in t.items()], [])
+def get_params(d, k=None):
+    tmp = map(lambda s: list(s.items())[0], d)
+    return itertools.chain.from_iterable([to_keyvalue_pairs(d, k, b) for b, d in tmp])
 
 
 def expand_configs_dict(configs_dict):
-    benchmark_dict = configs_dict.pop("benchmark")
-    benchmark_params = [
-        {"BENCHMARK": "polybench.source"},
-        {"BENCHMARK": "xsbench.source", "dataset": "mini"},
-        {"BENCHMARK": "xsbench.source", "dataset": "small"},
-    ]
     sim_dict = configs_dict.pop("sim_params")
-    sim_params = [
-        {"CACHELINE": "128", "CACHE_SIZE": "8MB"},
-        {"CACHELINE": "128", "CACHE_SIZE": "1MB"},
-    ]
-    get_sim_params(sim_dict)
-    tmp = sum([fun3(t) for t in benchmark_dict], [])
-    for t in tmp:
-        print("*", t)
+    sim_params = get_params(sim_dict)
+
+    benchmark_dict = configs_dict.pop("benchmark")
+    benchmark_params = get_params(benchmark_dict, "BENCHMARK")
 
     assert configs_dict == {}, "Incorrect yaml file"
 
@@ -82,7 +54,7 @@ def expand_configs_dict(configs_dict):
 
     # WARNING: do not remove the list conversion, because it will
     # return an iterator, which "disappears" after one use.
-    return list(map(lambda t: t[0] | t[1], prod))
+    return list(map(lambda t: t[0] + t[1], prod))
 
 
 def launch_on_all_cores(cmd, configs):
@@ -96,6 +68,8 @@ def launch_on_all_cores(cmd, configs):
 def main(args):
     configs_dict = yaml.load(open(args.path), Loader=yaml.SafeLoader)
     configs = expand_configs_dict(configs_dict)
+    for i, c in enumerate(configs):
+        print(i, c)
     if args.compile:
         launch_on_all_cores(COMPILE_CMD, configs)
     if args.runsim:

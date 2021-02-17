3a0de56ecbcd63a4ad78fd00aff76342a11635f5
Emil VATAI <emil.vatai@gmail.com>
2021-02-17 21:16:58 +0900

    Simplify polybench iteration script

--- utils/iterate_benchmarks.sh
+++ utils/iterate_benchmarks.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 SRCDIR=${SRCDIR:-"./polybench-c-4.2.1-beta"}
-LIST_FILE=${LIST_FILE:-"polybench_list.txt"}
+# LIST_FILE=${LIST_FILE:-"polybench_list.txt"}
 
-if [ -e ${LIST_FILE} ]; then
-    # If ${LIST_FILE} exists iterate uncommented lines.
-    sed -e '/^#/d' ${LIST_FILE}
-else
-    # Otherwise iterate all benchmarks.
-    for DIR in $(find ${SRCDIR} -type d); do
-        NAME=$(basename ${DIR})
-        if [ -f ${DIR}/${NAME}.c ]; then
-            echo ${DIR} | sed -e "s!${SRCDIR}/!!"
-        fi
-    done
-fi
+# if [ -e ${LIST_FILE} ]; then
+#     # If ${LIST_FILE} exists iterate uncommented lines.
+#     sed -e '/^#/d' ${LIST_FILE}
+# else
+# Otherwise iterate all benchmarks.
+for DIR in $(find ${SRCDIR} -type d); do
+    NAME=$(basename ${DIR})
+    if [ -f ${DIR}/${NAME}.c ]; then
+        echo ${DIR} | sed -e "s!${SRCDIR}/!!"
+    fi
+done
+# fi
 
 

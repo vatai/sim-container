789877f11cf6f2299bf654f78fd22577e3a1c584
Emil VATAI <vatai@silencio.localdomain>
2021-03-22 16:55:54 +0900

    Ncores

--- 03-run-benchmark.sh
+++ 03-run-benchmark.sh
@@ -5,6 +5,13 @@ source env.source
 
 umask 022; mkdir -m755 -p ${HOST_OUTDIR}
 
+HOST_ENV_FILE=${HOST_BINDIR}/${ENV_FILE}
+echo "OMP_NUM_THREADS=${NCORES}" > "$HOST_ENV_FILE"
+echo "OMP_NUM_PARALELL=${NCORES}" >> "$HOST_ENV_FILE"
+echo "FLIB_FASTOMP=FALSE" >> "$HOST_ENV_FILE"
+echo "FLIB_CNTL_BARRIER_ERR=FALSE" >> "$HOST_ENV_FILE"
+
+
 docker run --rm \
        --env BINDIR=${BINDIR} \
        --env OUTDIR=${OUTDIR} \

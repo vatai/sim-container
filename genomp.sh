56de44b59738e7bfe8ec52613429901f67100a89
Emil VATAI <emil.vatai@gmail.com>
2021-03-28 22:32:28 +0900

    Add `genomp.sh`

---
+++ genomp.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+FILENAME="omp$1.txt"
+echo OMP_NUM_THREADS=$1 > ${FILENAME}
+echo OMP_NUM_PARALELL=$1 >> ${FILENAME}
+echo FLIB_FASTOMP=FALSE >> ${FILENAME}
+echo FLIB_CNTL_BARRIER_ERR=FALSE >> ${FILENAME}
+
+echo ${FILENAME}

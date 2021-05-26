d079dbca6c84a58ba346520c48a7c02672cc49c3
Emil VATAI <emil.vatai@gmail.com>
2021-05-26 13:11:28 +0900

    Updated install script

--- fugaku-installer.sh
+++ fugaku-installer.sh
@@ -19,7 +19,7 @@ sed -i -e "s#PREFIX=/opt/riken_simulator#PREFIX=$ROOTDIR/dep/$BM#g" ./util/gem5-
 sed -i "369,372s:^:#:" ./SConstruct
 sed -i -e 's/ exit(/ sys.exit(/g' ./util/cpt_upgrader.py
 sed -i -e 's/if NO_FALLOCATE.*/if NO_FALLOCATE==0/' ./src/sim/syscall_emul.cc
-sed -i -e 's/const Addr PageShift = 16;/const Addr PageShift = 25;/' ./src/arch/arm/isa_traits.hh
+sed -i -e 's/const Addr PageShift = 25;/const Addr PageShift = 16;/' ./src/arch/arm/isa_traits.hh
 sed -i 's/typedef uint64_t SnoopMask;/typedef unsigned __int128 SnoopMask;/' ./src/mem/snoop_filter.hh
 sed -i '38 i std::ostream& operator<<(std::ostream& d, const unsigned __int128 v);' ./src/base/cprintf_formats.hh
 sed -i '41 i ostream& operator<<(ostream& d, const unsigned __int128 v) { d << "128int Hi:" << (void*)v << ";Lo:" << (void*)(v >> 64); return d;}' ./src/base/cprintf.cc

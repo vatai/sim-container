8aca72a4a20bbd2d8d05e64f586f73ce9a3ab657
Emil VATAI <emil.vatai@riken.jp>
2021-06-03 13:18:32 +0900

    Update `compile-polybench.sh`

--- utils/compile-polybench.sh
+++ utils/compile-polybench.sh
@@ -1,9 +1,10 @@
 CC=${CC:-gcc}
+CFLAGS=${CFLAGS:-"-O3"}
 DS=${DS:-MEDIUM_DATASET}
 BMDIR=${BMDIR:-./linear-algebra/solvers/gramschmidt}
-BM=${BM:-gramschmidt}
+BM=${BM:-$(basename $BMDIR)}
 
-$CC -O3 \
+$CC $CFLAGS \
 	-I ./utilities/ -I ./$BMDIR/$BM ./utilities/polybench.c \
 	$BMDIR/$BM.c -D$DS -DPOLYBENCH_TIME -lm \
 	-o $BM.$DS.$CC

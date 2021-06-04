a34f6d14cd49005b5d5ab4ab78985db162338a83
Emil VATAI <emil.vatai@gmail.com>
2021-06-04 11:04:18 +0900

    Save stderr too

--- run-dynamic.sh
+++ run-dynamic.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 BIN=$1
+BM=$(basename $1)
 
-./build/ARM/gem5.opt -d "m5/$(basename $BIN)" ./configs/example/se.py \
+./build/ARM/gem5.opt -d "m5/$BM" ./configs/example/se.py \
 	--cpu-type=O3_ARM_PostK_3 \
 	--caches --l2cache \
 	-e ../sim-container/fj_env.txt -n 1 \
 	-c ../sim-container/ld-linux-aarch64.so.1 \
-	-o ${BIN} > "m5/$(basename $BIN).output.txt"
+	-o ${BIN} \
+        1> "m5/${BM}.stdout.txt" \
+        2> "m5/${BM}.stderr.txt"

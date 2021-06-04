8622cfbd540a50e5f43875a4b3b60e6248c644a0
Emil VATAI <emil.vatai@gmail.com>
2021-06-04 15:23:07 +0900

    Add missing quotation marks

--- run-from-list.sh
+++ run-from-list.sh
@@ -5,5 +5,5 @@ list=$1
 cat $list | while read line; do
     bin=$(echo $line | cut -d\  -f1)
     core=$(echo $line | cut -d\  -f2)
-    screen -S $(basename $bin) -d -m bash -c ../sim-container/run-dynamic.sh $bin $core
+    screen -S $(basename $bin) -d -m bash -c "../sim-container/run-dynamic.sh $bin $core"
 done

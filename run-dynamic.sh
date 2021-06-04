f82bb8e29cca89a81aae9a2f4cbda2db6a8cc1c1
Emil VATAI <emil.vatai@gmail.com>
2021-06-04 10:47:16 +0900

    Clean up `run-dynamic.sh`

--- run-dynamic.sh
+++ run-dynamic.sh
@@ -2,4 +2,9 @@
 
 BIN=$1
 
-./build/ARM/gem5.opt -d "m5-$(basename $BIN)" ./configs/example/se.py --cpu-type=O3_ARM_PostK_3 --caches --l2cache -e ../sim-container/fj_env.txt -n 1 -c ../sim-container/ld-linux-aarch64.so.1 -o ${BIN}
+./build/ARM/gem5.opt -d "m5/$(basename $BIN)" ./configs/example/se.py \
+	--cpu-type=O3_ARM_PostK_3 \
+	--caches --l2cache \
+	-e ../sim-container/fj_env.txt -n 1 \
+	-c ../sim-container/ld-linux-aarch64.so.1 \
+	-o ${BIN} > "m5/$(basename $BIN).output.txt"

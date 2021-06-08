98c020a2397157db640071423e2df9aecf504f0f
Emil VATAI <emil.vatai@gmail.com>
2021-06-08 18:43:15 +0900

    Make everything fabulous!

--- visualise/sim-accuracy.py
+++ visualise/sim-accuracy.py
@@ -18,30 +18,14 @@ def get_stats_dict(m5dir):
     return dict(result)
 
 
-def get_stats_df(path):
-    path = Path(path).expanduser()
+def get_stats_df(path, m5dir_glob="*.fccpx"):
     entries = {}
-    for m5dir in path.glob("*.fccpx"):
+    for m5dir in path.glob(m5dir_glob):
         name = m5dir.name
         stats = get_stats_dict(m5dir)
         entries[name] = stats
     df = pd.DataFrame(entries).transpose()
-    df["source"] = path.name
-    return df
-
-
-def host_seconds_barplot():
-    bali_df = get_stats_df("~/bali-sim-m5")
-    rsim_df = get_stats_df("~/riken-sim-m5")
-    big_df = pd.concat([bali_df, rsim_df])
-    key = "host_seconds"
-    big_df[key] = pd.to_numeric(big_df[key])
-    df = big_df.pivot(columns=["source"], values="host_seconds")
-
-    print(df.head())
-    df.plot.bar()
-    plt.show()
-    plt.close()
+    return df.sort_index()
 
 
 def get_kernel_time(filename):
@@ -53,65 +37,107 @@ def get_kernel_time(filename):
                 pass
 
 
-def get_kernel_times_df(path):
-    path = Path(path).expanduser()
-    files = path.glob("*.fccpx.stdout.txt")
+def get_kernel_times_df(path, stdout_glob="*.fccpx.stdout.txt"):
+    files = path.glob(stdout_glob)
     result = {}
     for filename in files:
-        key = filename.name.split(".")[0]
+        key = ".".join(filename.name.split(".")[:3])
         result[key] = get_kernel_time(filename)
-    return pd.Series(result, name=path.name)
+    wtime = pd.Series(result, name="kernel_wtime")
+    df = pd.DataFrame(wtime)
+    return df.sort_index()
 
 
-def accuracy_plot():
-    a64fx = get_kernel_times_df("~/a64fx-outs")
-    bali = get_kernel_times_df("~/bali-sim-m5")
-    rsim = get_kernel_times_df("~/riken-sim-m5")
-    abs_times = pd.DataFrame([bali, a64fx]).transpose().sort_index()
-
-    fast_a = a64fx >= bali
-    slow_a = a64fx < bali
-    positive = a64fx[fast_a] / bali[fast_a] - 1
-    negative = bali[slow_a] / a64fx[slow_a] - 1
+def get_rel_abs_times(df, source1, key1, source2=None, key2=None):
+    source2 = source2 if source2 else source1
+    key2 = key2 if key2 else key1
+    data1 = df.loc[df["source"] == source1, key1].astype(float)
+    data2 = df.loc[df["source"] == source2, key2].astype(float)
+    assert source1 == source2 or key1 == key2
+    if source1 == source2:
+        data1.name = key1
+        data2.name = key2
+    else:
+        data1.name = source1
+        data2.name = source2
+    data1.index = data1.index.str.split(".").str[0]
+    data2.index = data2.index.str.split(".").str[0]
+    fast1 = data1 >= data2
+    fast2 = data1 < data2
+    positive = data2[fast2] / data1[fast2] - 1
+    negative = data1[fast1] / data2[fast1] - 1
     rel_times = pd.concat([positive, -negative]).sort_index()
+    abs_times = pd.DataFrame([data2, data1]).transpose().sort_index()
+    return rel_times, abs_times
 
-    sims = bali - rsim
-    print(f"min: {sims.min()}, max: {sims.max()}")
 
-    # bar_plot = df.plot.bar()
-    # fig = bar_plot.get_figure()
-    # fig.savefig("bars", bbox_inches="tight")
-    # plt.clf()
-
-    # speedup_plot = rel_times.plot.bar()
-    # fig = speedup_plot.get_figure()
-    # fig.savefig("speedup", bbox_inches="tight")
-    # plt.clf()
-
+def speedup_plot(title, savefig, rel_times, abs_times):
     rel_color = "green"
     dot_color = "lightgreen"
     fig, ax0 = plt.subplots()
     ax1 = ax0.twinx()
 
-    ax0.set(
-        title="Absolute and relative comparison of sim and chip \n"
-        "of polybench benchmarks, only kernels, medium dataset, fccpx"
-    )
+    margin = 1.3
+    top1 = max(rel_times) * margin
+    bot1 = min(rel_times) * margin
+    top0 = abs_times.max()[0] * margin
+    bot0 = min(0, top0 * bot1 / top1)
+    ax0.set(title=title)
     ax0.set_ylabel("Absolute speed (in seconds)")
-    ax0.set_ylim(-0.05, 0.5)
+    ax0.set_ylim(bot0, top0)
     ax1.set_ylabel("Relative speed (+1: 2x speedup, -1: 2x slowdown)", color=rel_color)
-    ax1.set_ylim(-0.22, 2.20)
+    ax1.set_ylim(bot1, top1)
     ax1.tick_params(axis="y", labelcolor=rel_color)
 
     abs_times.plot.bar(ax=ax0)
     rel_times.plot.line(marker="D", color=dot_color, linestyle="None", ax=ax1)
     ax1.axhline(linestyle="dashed", linewidth=1, color=rel_color)
+    mean = rel_times.abs().mean()
+    ax1.axhline(mean, linestyle="dotted", color=rel_color)
+    ax1.text(
+        1.0,
+        mean,
+        f"mean: {mean:0.06}",
+        va="center",
+        color=rel_color,
+        backgroundcolor="w",
+    )
 
     fig.tight_layout()
-    # plt.show()
-    plt.savefig("combined")
+    plt.savefig(savefig)
+    plt.show()
     plt.close()
 
 
-host_seconds_barplot()
-accuracy_plot()
+def get_host_seconds_wtime_df(m5dir):
+    m5dir = Path(m5dir).expanduser()
+    df1 = get_stats_df(m5dir)
+    df2 = get_kernel_times_df(m5dir)
+    df = pd.concat([df1, df2], axis=1)
+    df["source"] = m5dir.name
+    return df
+
+
+def main():
+    dirs = [
+        "~/bali-sim-m5",
+        "~/riken-sim-m5",
+        "~/a64fx-outs",
+    ]
+    df = pd.concat(map(get_host_seconds_wtime_df, dirs))
+
+    title = "Simulation accuracy: chip vs sim kernel wtime"
+    times = get_rel_abs_times(df, "bali-sim-m5", "kernel_wtime", "a64fx-outs")
+    speedup_plot(title, "sim_accuracy.pdf", *times)
+
+    title = "Simulation slowdown"
+    times = get_rel_abs_times(df, "bali-sim-m5", "sim_seconds", key2="host_seconds")
+    speedup_plot(title, "sim_slowdown.pdf", *times)
+
+    title = "Full vs kernel wtime (on sim)"
+    times = get_rel_abs_times(df, "bali-sim-m5", "kernel_wtime", key2="sim_seconds")
+    speedup_plot(title, "kernel_ratio.pdf", *times)
+
+
+if __name__ == "__main__":
+    main()

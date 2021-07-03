688401e992536ad856651ff4df1804998f8aad72
Emil VATAI <emil.vatai@gmail.com>
2021-07-04 00:00:26 +0900

    Better run script

--- run-dynamic.sh
+++ run-dynamic.sh
@@ -9,6 +9,7 @@ shift
 shift
 
 echo "xxx ${BIN} $@ xxx"
+ls
 
 if [[ -z $GEM5DIR ]]; then
   echo 'Please set $GEM5DIR first'
@@ -23,15 +24,15 @@ suffix=$(echo $@ | sed 's/ /_/g')
 out_path=${BM}${suffix}_numthreads$num_threads
 
 
-fjenv=/tmp/fjenv${num_threads}.txt
-sed s/THREADS=.*/THREADS=${num_threads}/ ../sim-container/fj_env.txt > $fjenv
+fjenv=/tmp/${BM}${core}fjenv${num_threads}.txt
+echo -e "LD_LIBRARY_PATH=${GEM5DIR}/../lib\nOMP_NUM_THREADS=${num_threads}\nFLIB_USE_CPURESOURCE_LIBOMP=FALSE" > $fjenv
 
-$pin_cmd $GEM5DIR/build/ARM/gem5.opt -d "m5/${out_path}" $GEM5DIR/configs/example/se.py \
+$pin_cmd $GEM5DIR/build/ARM/gem5.opt -d "${GEM5DIR}/m5/${out_path}" $GEM5DIR/configs/example/se.py \
 	--cpu-type=O3_ARM_PostK_3 \
 	--caches --l2cache \
 	--mem-size=32GB \
 	-e $fjenv -n $num_threads \
-	-c ../sim-container/ld-linux-aarch64.so.1 \
+	-c ${GEM5DIR}/../sim-container/ld-linux-aarch64.so.1 \
 	-o "${BIN} $@" \
-        1> "m5/${out_path}.stdout.txt" \
-        2> "m5/${out_path}.stderr.txt"
+        1> "${GEM5DIR}/m5/${out_path}.stdout.txt" \
+        2> "${GEM5DIR}/m5/${out_path}.stderr.txt"

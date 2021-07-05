b350f9f3c98ee0d1cb0c7d1a45296dad5a6e7673
Emil VATAI <emil.vatai@gmail.com>
2021-07-05 17:56:57 +0900

    Update `run-dynamic.sh`

--- run-dynamic.sh
+++ run-dynamic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+script_name=$0
 core=$1
 num_threads=$2
 BIN=$3
@@ -7,9 +8,6 @@ BM=$(basename $BIN)
 shift
 shift
 shift
-
-echo "xxx ${BIN} $@ xxx"
-ls
 
 if [[ -z $GEM5DIR ]]; then
   echo 'Please set $GEM5DIR first'
@@ -20,9 +18,12 @@ if [[ "" -ne $core ]]; then
   pin_cmd="numactl -C $core"
 fi
 
-suffix=$(echo $@ | sed 's/ /_/g')
-out_path=${BM}${suffix}_numthreads$num_threads
+l2_bankbit=${l2_bankbit:-2}
+l2_bus_width=${l2_bus_width:-64}
+l2_resp_width=${l2_resp_width:-128}
 
+suffix=$(echo $@ | sed 's/ /_/g')
+out_path=${BM}${suffix}_numthreads${num_threads}_bankbit${l2_bankbit}_buswidth${l2_bus_width}_respwidth${l2_resp_width}
 
 fjenv=/tmp/${BM}${core}fjenv${num_threads}.txt
 echo -e "LD_LIBRARY_PATH=${GEM5DIR}/../lib\nOMP_NUM_THREADS=${num_threads}\nFLIB_USE_CPURESOURCE_LIBOMP=FALSE" > $fjenv
@@ -31,8 +32,12 @@ $pin_cmd $GEM5DIR/build/ARM/gem5.opt -d "${GEM5DIR}/m5/${out_path}" $GEM5DIR/con
 	--cpu-type=O3_ARM_PostK_3 \
 	--caches --l2cache \
 	--mem-size=32GB \
+	--l2_bankbit=${l2_bankbit} \
+	--l2_bus_width=${l2_bus_width} \
+	--l2_resp_width=${l2_resp_width} \
 	-e $fjenv -n $num_threads \
-	-c ${GEM5DIR}/../sim-container/ld-linux-aarch64.so.1 \
+	-c ${GEM5DIR}/../lib/ld-linux-aarch64.so.1 \
 	-o "${BIN} $@" \
         1> "${GEM5DIR}/m5/${out_path}.stdout.txt" \
         2> "${GEM5DIR}/m5/${out_path}.stderr.txt"
+
